a56ae71aa964b700dc70ad1a2469761d5e5254ac
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2020-07-16 22:56:09 -0700

    add LinkedLists

---
+++ src/LinkedLists/LinkedList.java
@@ -0,0 +1,38 @@
+package LinkedLists;
+
+public class LinkedList<E> {
+
+    //inner class to restrict access
+    class Node<E> {
+        E data;
+        Node<E> next;
+        public Node(E obj) {
+            data = obj;
+            next = null;
+        }
+    }
+    private Node<E> head;
+    private int currentSize; //O(1) access to size of list, w/out it, we have O(n)
+
+    public LinkedList(){
+        head = null;
+        currentSize = 0;
+    }
+    /*
+    boundary conditions:
+    emptying
+    finding single element
+    adding/removing from front
+    adding/removing from end
+    working in middle
+      */
+
+    public void addFirst(E obj) {
+        Node<E> node = new Node<E>(obj);
+        node.next = head; //scoot head one spot over
+        //so now head is the new node
+        head = node;
+        currentSize++;
+
+    }
+}
