95ba7658ee58b68b31c8c6e30a68f4fd39550f65
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2020-07-26 19:45:11 -0700

    updated CircularLinkedList constructor

--- src/LinkedLists/CircularLinkedList/Circular.java
+++ src/LinkedLists/CircularLinkedList/Circular.java
@@ -19,15 +19,25 @@ class Node {
 }
 public class Circular {
     Node last;
-//    public CircularLinkedList(Node last) {
-//        this.last = last;
-//        if (this.last != null) {
-//            this.last.next = last;
-//        }
-//    }
-    /*
-    1 -> 2 -> 3 -> 1
-     */
+    public Circular(Node last) {
+        this.last = last;
+        if (this.last != null) {
+            this.last.next = last; //
+        }
+        //head -> last -> n
+        //1 -> 2 -> 3 -> 1
+        last = new Node(2);
+        Node n = new Node(3);
+        //new node's is placed in between 2 and 1
+        // new node's next pointer points to what tail points to
+        n.next = last.next;
+        //tail's pointer now points at new node
+        //2 -> 3
+        last.next = n;
+        //new node is now the tail
+        last = n;
+    }
+
 
     void addToEnd(int data) {
         Node n = new Node(data);
