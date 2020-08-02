f9f473d3804edcb439f75db30c3e17a132ea7537
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2020-08-02 15:36:00 -0700

    updated hashtable without collision handling

--- src/HashTable/SimpleHashtable.java
+++ src/HashTable/SimpleHashtable.java
@@ -7,16 +7,30 @@ public class SimpleHashtable {
 
     }
     public void put(String key, Employee employee) {
+        //doesn't handle collision
+        //give index in the array
         int hashedKey = hashKey(key);
         if (hashtable[hashedKey] != null) {
-            System.out.println("sorry already an emp at position, cant handle collision" + hashedKey);
+            System.out.println("already an emp at position, cant handle collision" + hashedKey);
         } else {
             hashtable[hashedKey] = employee;
         }
     }
+    //retrieval is O(1)
+    public Employee get(String key) {
+        int hashedKey = hashKey(key);
+        return hashtable[hashedKey];
+    }
     private int hashKey(String key) {
         // 5 mod 10 = 5
         return key.length() % hashtable.length;
+    }
+
+
+    public void printHashtable() {
+        for (int i = 0; i < hashtable.length; i++) {
+            System.out.println(hashtable[i]);
+        }
     }
 
 }
