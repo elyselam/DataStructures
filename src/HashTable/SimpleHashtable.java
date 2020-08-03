4f16823110c247f414f43f446f458e8afd85b60d
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2020-08-03 16:41:04 -0700

    added hashTable linear probing

--- src/HashTable/SimpleHashtable.java
+++ src/HashTable/SimpleHashtable.java
@@ -1,36 +1,55 @@
 package HashTable;
 
 public class SimpleHashtable {
-    private Employee[] hashtable;
+    private Employee[] arr;
     public SimpleHashtable() {
-        hashtable = new Employee[10];
-
+        //arr[5] = 'jane jones'
+        arr = new Employee[10];
     }
+
     public void put(String key, Employee employee) {
-        //doesn't handle collision
-        //give index in the array
-        int hashedKey = hashKey(key);
-        if (hashtable[hashedKey] != null) {
-            System.out.println("already an emp at position, cant handle collision" + hashedKey);
+        int k = hashKey(key);
+        //if occupied, do linear probing
+        if (occupied(k)) {
+            //loop around until we find an open spot
+            int stopIndex = k; //smith => 5
+            //if 5 == 10-1, wraps around by setting it to 0
+            //if index is 9, wrap around
+            if (k == arr.length - 1) {
+                k = 0;
+            } else {
+                k++; //now index 5 is 6
+            }
+            //keeps incrementing if the next slot is also occupied
+            while (occupied(k) && k != stopIndex) {
+                k = (k + 1) % arr.length;
+            }
+        }
+        //occupied method
+        if (occupied(k)) {
+            System.out.println("already an emp at position, cant handle collision" + k);
         } else {
-            hashtable[hashedKey] = employee;
+            arr[k] = employee;
         }
     }
     //retrieval is O(1)
     public Employee get(String key) {
-        int hashedKey = hashKey(key);
-        return hashtable[hashedKey];
+        int k = hashKey(key);//5
+        return arr[k]; //arr[5] = 'jane jones'
     }
     private int hashKey(String key) {
-        // 5 mod 10 = 5
-        return key.length() % hashtable.length;
+        // 'jones'.length => 5 % 10 = 5
+        return key.length() % arr.length;
     }
 
+    //if there is already there, it's occupied
+    private boolean occupied(int index) {
+        return arr[index] != null;
+    }
 
     public void printHashtable() {
-        for (int i = 0; i < hashtable.length; i++) {
-            System.out.println(hashtable[i]);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
         }
     }
-
 }
